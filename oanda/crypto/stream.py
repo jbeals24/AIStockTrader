2d29fb58d9c913ab832b55e147e930c5fbfedd8a
jbeals24 <103008158+jbeals24@users.noreply.github.com>
2024-06-07 16:36:02 -0400

    Update stream.py

--- oanda/crypto/stream.py
+++ oanda/crypto/stream.py
@@ -4,19 +4,18 @@ from oandapyV20.exceptions import V20Error
 from oandapyV20.endpoints.pricing import PricingStream
 
 def stream_crypto_pricing(account_id, access_token, instruments):
-    api = API(access_token=access_token, environment="live")
+    api = API(access_token=access_token, environment="live") # change to non live account here if your subscription is for demo trading
     s = PricingStream(accountID=account_id, params={"instruments": instruments})
     try:
         for data in api.request(s):
-            # Extract bid and ask prices directly from the dictionary
             print(data)
     except V20Error as e:
         print("Error: {}".format(e))
 
 if __name__ == "__main__":
     # Replace the placeholders with your actual account ID, access token, and list of cryptocurrency instruments
-    accountID = "001-001-11990763-002"
-    access_token = "d5ab50dfd0d301a09f711297480b7f74-25c1b0e037151c7cbc7bebfac3049322"
+    accountID = "YOUR ACCOUNT ID HERE"
+    access_token = "YOUR ACCESS TOKEN HERE"
     instruments = "BTC_USD"  # Example list of cryptocurrency instruments
     
     # Start streaming cryptocurrency pricing data
