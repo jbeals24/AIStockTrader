01d2800e3b2ffefdefca795332a45041503776d9
jbeals24 <103008158+jbeals24@users.noreply.github.com>
2024-06-07 16:39:04 -0400

    Update test.py

--- oanda/forex/test.py
+++ oanda/forex/test.py
@@ -6,9 +6,6 @@ import random
 import string
 import sys
 
-# ACCOUNT_ID = '001-001-11990763-002'
-# ACCESS_TOKEN = 'd5ab50dfd0d301a09f711297480b7f74-25c1b0e037151c7cbc7bebfac3049322'
-
 firstAsk = 0
 firstBid = 0
 counter= 0
@@ -17,10 +14,8 @@ def stream_pricing_data(account_id, access_token, instruments, file):
     api = API(access_token=access_token, environment="live")
     s = PricingStream(accountID=account_id, params={"instruments": instruments})
     try:
-        
         for data in api.request(s):
             # Extract bid and ask prices directly from the dictionary
-            
             if 'bids' in data:
                 bid_price = float(data['bids'][0]['price'])
                 ask_price = float(data['asks'][0]['price'])
@@ -37,8 +32,8 @@ def stream_pricing_data(account_id, access_token, instruments, file):
 
 if __name__ == "__main__":
     # Replace the placeholders with your actual account ID and access token
-    accountID = "001-001-11990763-002"
-    access_token = "d5ab50dfd0d301a09f711297480b7f74-25c1b0e037151c7cbc7bebfac3049322"
+    accountID = "YOUR ACCOUNT ID HERE"
+    access_token = "YOUR ACCESS TOKEN HERE"
     instruments = "EUR_USD"  # Example list of instruments
     
     filePath = 'data/'
